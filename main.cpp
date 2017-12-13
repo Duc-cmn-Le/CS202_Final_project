8c3a540806c1fea54313827bf78e4be703a67c79
Le Duong Cong Duc <congduc296@gmail.com>
2017-12-14 01:24:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 #include <gdiplus.h>
 
 using namespace Gdiplus;
+/*
+int main() {
+    Database data_system;
+    std::vector<Shape*> _shape;
+    project::inputFromFile(_shape, "duc.svg", data_system);
+    for (int size=_shape.size(), i=0; i < size; i++)
+    {
+        _shape[i]->what_is_this();
+        _shape[i]->show_linker_data(); 
+    }
+    return 0;
+}
+*/
+
 #pragma comment (lib,"Gdiplus.lib")
 
 VOID OnPaint(HDC hdc)
